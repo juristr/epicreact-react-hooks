00cc35c098702a8dc006e64aa3d1d3f339cc32fb
Juri <juri.strumpflohner@gmail.com>
2020-10-04 00:03:48 +0200

    ex2 - persistent hooks

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,14 +3,42 @@
 
 import React from 'react'
 
-function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
+function useLocalStorageState(key, defaultValue = '') {
+  const [state, setState] = React.useState(() => {
+    const value = window.localStorage.getItem(key)
+    if (value) {
+      try {
+        return JSON.parse(window.localStorage(key))
+      } catch {
+        window.localStorage.removeItem(key)
+      }
+    }
 
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+    return typeof defaultValue === 'function' ? defaultValue() : defaultValue
+  })
+
+  React.useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(state))
+  }, [key, state])
+
+  return [state, setState]
+}
+
+function useCustomHook(initialName) {
+  const [name, setName] = React.useState(
+    window.localStorage.getItem('name') || initialName,
+  )
+
+  React.useEffect(() => {
+    window.localStorage.setItem('name', name)
+  })
+
+  return [name, setName]
+}
+
+function Greeting({initialName = ''}) {
+  const [name, setName] = useLocalStorageState('name', initialName)
+  // const [name, setName] = useCustomHook(initialName)
 
   function handleChange(event) {
     setName(event.target.value)
@@ -19,7 +47,7 @@ function Greeting({initialName = ''}) {
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name} />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
